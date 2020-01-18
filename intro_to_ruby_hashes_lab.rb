ea7d4d96dd167a043f015c4f1015d6c407a9d36a
Benjamin Sullivan <benjamin.r.sullivan@gmail.com>
2020-01-18 14:26:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,9 +18,9 @@ def monopoly_with_third_tier
 	    :pieces => 4,
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
-	      :two_piece_owned => {},
-	      :three_piece_owned => {},
-	      :four_piece_owned => {}
+	      :two_pieces_owned => {},
+	      :three_pieces_owned => {},
+	      :four_pieces_owned => {}
 	    },
 	    :names => {
   	    :reading_railroad => {},
